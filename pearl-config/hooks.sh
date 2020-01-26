afa7eafa68ae646231fed85740e5640e5aa24918
Filippo Squillace <squillace.filippo@gmail.com>
2020-01-26 23:15:41 +0100

    Remove emerge of dependency in hook function

--- pearl-config/hooks.sh
+++ pearl-config/hooks.sh
@@ -1,21 +1,4 @@
 function post_install(){
-    pearl emerge ${PEARL_PKGREPONAME}/bufexplorer
-    pearl emerge ${PEARL_PKGREPONAME}/dict
-    pearl emerge ${PEARL_PKGREPONAME}/dot-vim
-    pearl emerge ${PEARL_PKGREPONAME}/fugitive
-    pearl emerge ${PEARL_PKGREPONAME}/gitgutter
-    pearl emerge ${PEARL_PKGREPONAME}/indentline
-    pearl emerge ${PEARL_PKGREPONAME}/nerdcommenter
-    pearl emerge ${PEARL_PKGREPONAME}/nerdtree
-    pearl emerge ${PEARL_PKGREPONAME}/powerline
-    # Temporarily disabled:
-    #pearl emerge ${PEARL_PKGREPONAME}/python-mode
-    pearl emerge ${PEARL_PKGREPONAME}/supertab
-    pearl emerge ${PEARL_PKGREPONAME}/syntastic
-    pearl emerge ${PEARL_PKGREPONAME}/vim-colors
-    pearl emerge ${PEARL_PKGREPONAME}/vim-multiple-cursors
-    pearl emerge ${PEARL_PKGREPONAME}/vim-orgmode
-
     return 0
 }
 
@@ -26,24 +9,5 @@ function post_update(){
 }
 
 function pre_remove(){
-    if ask "Do you want to remove all vim related packages?" "N"
-    then
-        pearl remove ${PEARL_PKGREPONAME}/bufexplorer
-        pearl remove ${PEARL_PKGREPONAME}/dict
-        pearl remove ${PEARL_PKGREPONAME}/dot-vim
-        pearl remove ${PEARL_PKGREPONAME}/fugitive
-        pearl remove ${PEARL_PKGREPONAME}/gitgutter
-        pearl remove ${PEARL_PKGREPONAME}/indentline
-        pearl remove ${PEARL_PKGREPONAME}/nerdcommenter
-        pearl remove ${PEARL_PKGREPONAME}/nerdtree
-        pearl remove ${PEARL_PKGREPONAME}/powerline
-        # Temporarily disabled:
-        #pearl remove ${PEARL_PKGREPONAME}/python-mode
-        pearl remove ${PEARL_PKGREPONAME}/supertab
-        pearl remove ${PEARL_PKGREPONAME}/syntastic
-        pearl remove ${PEARL_PKGREPONAME}/vim-colors
-        pearl remove ${PEARL_PKGREPONAME}/vim-multiple-cursors
-        pearl remove ${PEARL_PKGREPONAME}/vim-orgmode
-    fi
     return 0
 }
